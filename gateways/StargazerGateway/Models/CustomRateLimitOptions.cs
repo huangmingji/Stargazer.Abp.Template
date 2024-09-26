dedbda54ecf31441d4ab6c79cbd2de6cf12f7cfd
huangmingji <huangmingji@stargazer.tech>
2024-09-26 12:03:05 +0800

    网关新增跨域、缓存、限流配置

---
+++ gateways/StargazerGateway/Models/CustomRateLimitOptions.cs
@@ -0,0 +1,14 @@
+namespace StargazerGateway.Models;
+public class CustomRateLimitOptions
+{
+    public const string CustomRateLimit = "CustomRateLimit";
+    public int PermitLimit { get; set; } = 100;
+    public int Window { get; set; } = 10;
+    public int ReplenishmentPeriod { get; set; } = 2;
+    public int QueueLimit { get; set; } = 2;
+    public int SegmentsPerWindow { get; set; } = 8;
+    public int TokenLimit { get; set; } = 10;
+    public int TokenLimit2 { get; set; } = 20;
+    public int TokensPerPeriod { get; set; } = 4;
+    public bool AutoReplenishment { get; set; } = false;
+}
--- gateways/StargazerGateway/Program.cs
+++ gateways/StargazerGateway/Program.cs
@@ -1,9 +1,85 @@
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+using StargazerGateway.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+// builder.Services.Configure<CustomRateLimitOptions>(builder.Configuration.GetSection(CustomRateLimitOptions.CustomRateLimit));
+var rateLimitOptions = new CustomRateLimitOptions();
+// builder.Configuration.GetSection(CustomRateLimitOptions.CustomRateLimit).Bind(rateLimitOptions);
+
+#region CORS
+string DefaultCorsPolicyName = "DefaultCors";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(DefaultCorsPolicyName, policy =>
+    {
+        string[] origins = new[] { "*" };
+        string corsOrigins = builder.Configuration["App:CorsOrigins"] ?? "";
+        if (!string.IsNullOrWhiteSpace(corsOrigins))
+        {
+            origins = corsOrigins.Split(",", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(x => x.Trim())
+                                .Select(x =>
+                                {
+                                    return x.EndsWith("/", StringComparison.Ordinal) ? x.Substring(0, x.Length - 1) : x;
+                                }).ToArray();
+
+            policy = policy.WithOrigins(origins);
+        }
+        else
+        {
+            policy = policy.AllowAnyOrigin();
+        }
+        policy.SetIsOriginAllowedToAllowWildcardSubdomains()
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+#endregion
+
+#region cache
+string DefaultCachePolicyName = "DefaultCache";
+builder.Services.AddOutputCache(options =>
+{
+    string expire = builder.Configuration["App:CacheExpire"] ?? "20";
+    int expireSeconds = int.Parse(expire);
+    options.AddPolicy("NoCache", builder => builder.NoCache());
+    options.AddPolicy(DefaultCachePolicyName, builder => builder.Expire(TimeSpan.FromSeconds(20)));
+    options.AddPolicy("CustomCache", builder => builder.Expire(TimeSpan.FromSeconds(expireSeconds)));
+});
+
+#endregion
+
+#region
+string DefaultRateLimiterPolicyName = "DefaultRateLimiter";
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter(DefaultRateLimiterPolicyName, opt =>
+    {
+        opt.PermitLimit = 4;
+        opt.Window = TimeSpan.FromSeconds(12);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.QueueLimit = 2;
+    });
+    options.AddFixedWindowLimiter("CustomRateLimiter", opt =>
+    {
+        opt.PermitLimit = rateLimitOptions.PermitLimit;
+        opt.Window = TimeSpan.FromSeconds(rateLimitOptions.Window);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.QueueLimit = rateLimitOptions.QueueLimit;
+    });
+});
+#endregion
+
 var app = builder.Build();
+app.UseCors();
+app.UseOutputCache();
+app.UseRateLimiter();
 app.MapReverseProxy();
 
 app.Run();
